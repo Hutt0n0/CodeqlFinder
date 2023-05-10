98993213e0e608d0e7807d6ef3d37d6cfb4a66ea
Hutt0n0 <1598677824@qq.com>
2023-05-10 16:40:45 +0800

    fix

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
  * @version:
  * @Author: Hutt0n0
  * @Date: 2023-01-25 14:52:46
- * @LastEditTime: 2023-05-08 14:07:40
+ * @LastEditTime: 2023-05-10 16:39:40
  */
 package main
 
@@ -35,18 +35,18 @@ func main() {
 
 	flag.StringVar(&database, "d", "", "编译后的数据库文件目录，默认为空")
 	flag.StringVar(&sourceCode, "s", "", "源码位置，默认为空")
-	flag.BoolVar(&skipEnv, "skip", false, "是否跳过环境监察，跳过加上-skip true，默认是不跳过")
+	flag.BoolVar(&skipEnv, "skip", false, "是否跳过环境监察，跳过加上-skip，默认是不跳过")
 	flag.StringVar(&qlpath, "q", "", "指定ql目录路径或者指定单个ql脚本，默认为空")
 	flag.StringVar(&cmd, "cmd", "mvn clean package -DskipTests=true", "maven编译命令，默认是mvn clean package -DskipTests=true")
-	flag.BoolVar(&static, "static", false, "若只寻找sink点，需要加上-static true。默认为污点利用链分析")
+	flag.BoolVar(&static, "static", false, "若只寻找sink点，需要加上-static 。默认为污点利用链分析")
 	flag.Parse()
-	lengthArgs := len(flag.Args())
-	if lengthArgs <= 0 {
+
+	if database == "" {
 		f := flag.Usage
 		f()
-		fmt.Println("第一次执行推荐检查环境：./CodeqlFinder -d 数据库文件目录 -q ql文件目录路径 -static true")
-		fmt.Println("直接运行：./CodeqlFinder -d 数据库文件目录 -q ql文件目录路径 -static true -skip true")
-		fmt.Println("编译源码：./CodeqlFinder -d 存放数据库文件目录 -s 要编译的源码位置  -skip true -cmd maven编译命令[可选] ")
+		fmt.Println("第一次执行推荐检查环境：./CodeqlFinder -d 数据库文件目录 -q ql文件目录路径 -static")
+		fmt.Println("直接运行：./CodeqlFinder -d 数据库文件目录 -q ql文件目录路径 -static -skip ")
+		fmt.Println("编译源码：./CodeqlFinder -d 存放数据库文件目录 -s 要编译的源码位置  -cmd maven编译命令[可选] -skip")
 		return
 	}
 	compile = false
